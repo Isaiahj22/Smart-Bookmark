446498931e06a105c0bd7f2f5fb25b9fc0e20e2c
Isaiahj22 <168603714+Isaiahj22@users.noreply.github.com>
2025-04-26 00:58:40 -0600

    Update app.js
    Connects button to back end

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,6 @@
+document.getElementById('tipButton').addEventListener('click', async () => {
+    const response = await fetch('http://localhost:5000/get-tip');
+    const data = await response.json();
+    document.getElementById('tipArea').innerText = data.tip;
+});
 
